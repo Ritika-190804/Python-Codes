1a07696b7628966db8d84e71fd90720559b3da78
Ritika-190804 <iamritika2004@gmail.com>
2024-03-13 22:34:20 +0530

    string-part-1

---
+++ stringmanipulation.py
@@ -0,0 +1,40 @@
+a="Harry Potter and the Goblet of Fire"
+print(a)
+
+#to find the length of the string
+
+print(len(a))
+
+#to find the number of times a character is occuring
+print(a.count("H"))
+
+#to convert each letter into upper case
+print(a.upper())
+
+#to convert each letter into lower case
+print(a.lower())
+
+#to find the index of any character
+print(a.index("o",15,34))
+
+#to convert the first letter into capital
+print(a.capitalize())
+
+#to convert the string into lower case
+print(a.casefold())
+
+#to find the index number of a character
+print(a.find("o",15,34))
+
+#to write variables inside a string
+name="john"
+b="my name is {}"
+print(b.format(name))
+
+#it fills the given character and centralize a string
+print(name.center(20,"*"))
+
+
+
+
+
